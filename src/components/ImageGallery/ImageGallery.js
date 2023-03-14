a8842af77c1451179456fb04d7c016a0a14e2e09
RuslanTenderyts <rylya1995@gmail.com>
2023-03-15 00:28:28 +0200

    hw-hook

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,29 +1,29 @@
-// import React from "react";
-// import PropTypes from "prop-types";
-// import { Gallery, Layout } from './ImageGallery.styled'
-// import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem'
+import React from "react";
+import PropTypes from "prop-types";
+import { Gallery, Layout } from './ImageGallery.styled'
+import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem'
 
 
-// export const ImageGallery = ({ images }) => {
-//   return (
-//     <Layout>
-//         <Gallery>
-//         {images.map(({id, webformatURL, tags, largeImageURL}) => (
-//             <ImageGalleryItem key={id} url={webformatURL} tags={tags} largeImage={largeImageURL} />
-//         ))}
+export const ImageGallery = ({ images }) => {
+  return (
+    <Layout>
+        <Gallery>
+        {images.map(({id, webformatURL, tags, largeImageURL}) => (
+            <ImageGalleryItem key={id} url={webformatURL} tags={tags} largeImage={largeImageURL} />
+        ))}
         
-//         </Gallery>
-//     </Layout>
-//   )
-// }
+        </Gallery>
+    </Layout>
+  )
+}
 
 
-// PropTypes.ImageGallery = {
-//   images: PropTypes.arrayOf
-//     (PropTypes.shape({ 
-//       id: PropTypes.string.isRequired,
-//       url: PropTypes.string.isRequired,
-//       largeImage: PropTypes.string.isRequired,
-//       tags: PropTypes.string.isRequired, })
-//     ).isRequired, 
-// };
+PropTypes.ImageGallery = {
+  images: PropTypes.arrayOf
+    (PropTypes.shape({ 
+      id: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      largeImage: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired, })
+    ).isRequired, 
+};
